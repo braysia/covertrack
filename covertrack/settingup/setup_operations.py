b2325da50980ba0ee483f43429de9c5dd47507c0
braysia <kudo@DN0a22b50d.SUNet>
2016-09-13 16:54:30 -0700

    add first_frame and last_frame

--- covertrack/settingup/setup_operations.py
+++ covertrack/settingup/setup_operations.py
@@ -6,7 +6,8 @@ import os
 def retrieve_files(argdict, imgdir, holder, channels):
     chdict = {}
     for ch in channels:
-        chdict[ch] = sorted([join(imgdir, i) for i in os.listdir(imgdir) if ch in i])
+        pathset = [join(imgdir, i) for i in os.listdir(imgdir) if ch in i]
+        chdict[ch] = sorted(pathset)[argdict['first_frame']:argdict['last_frame']]
     argdict['channeldict'] = chdict
     argdict['channels'] = channels
     return argdict
@@ -15,7 +16,8 @@ def retrieve_files(argdict, imgdir, holder, channels):
 def retrieve_files_glob(argdict, imgdir, holder, channels, patterns):
     chdict = {}
     for ch, pattern in zip(channels, patterns):
-        chdict[ch] = glob.glob(join(imgdir, pattern))
+        pathset = glob.glob(join(imgdir, pattern))
+        chdict[ch] = pathset[argdict['first_frame']:argdict['last_frame']]
     argdict['channeldict'] = chdict
     argdict['channels'] = channels
     return argdict
